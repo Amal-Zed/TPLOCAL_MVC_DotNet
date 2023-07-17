38285b65937980350f244bfe8bf142807c0c8bad
Amal ZED <zedda.amal@gmail.com>
2023-07-17 10:15:37 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+    
+      //methode "naturally" call by router
+        public IActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "AvisList":
+                        //TODO : code reading of the xml files provide
+                        return View(id,new OpinionList().GetAvis("XlmFile/DataAvis.xml"));
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //methode to send datas from form to validation page
+        [HttpPost]
+        public IActionResult FormValidation(Dataform data)
+        {
+
+                if (ModelState.IsValid)
+                {
+                return View(data);
+                }
+                return View("FormValidation", data);
+        }
+
+    }
+}
---
+++ Models/Dataform.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace TPLOCAL1.Models
+{
+    public class Dataform
+    {
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required(ErrorMessage = "Selectionner un sexe")]
+        public string Sexe { get; set; }
+        [MinLength(5)]
+        public string Addresse { get; set; }
+        [RegularExpression("\\d{5}", ErrorMessage = "Doit contenir cinq chiffres exp : 92320")]
+        
+        public int CodePostal { get; set; }
+        public string Ville { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime DateDeb { get; set; }
+        [Required]
+        public string Formation { get; set; }
+        [Required]
+        public string? CobolAvis { get; set; }
+        [Required]
+        public string? FormObj { get; set; }
+
+    }
+
+ 
+}
